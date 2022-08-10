535710293f61ceec1f740d33f75b9e7bc4527221
Nightfly-student <70700436+Nightfly-student@users.noreply.github.com>
2022-08-10 11:12:27 +0200

    maybe fix gtag

--- plugins/vue-gtag-next.client.ts
+++ plugins/vue-gtag-next.client.ts
@@ -1,10 +1,12 @@
-import { defineNuxtPlugin } from "#app";
-import VueGtag from 'vue-gtag-next'
+import VueGtag from "vue-gtag-next";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: 'UA-197110880-2'
-    }
-  })
-})
+      id: "UA-197110880-2",
+      params: {
+        anonymize_ip: true,
+      },
+    },
+  });
+});
