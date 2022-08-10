53df53492abb0a9a9f691803cca18c5c87985153
Nightfly-student <70700436+Nightfly-student@users.noreply.github.com>
2022-08-10 11:22:37 +0200

    test

--- plugins/vue-gtag-next.client.ts
+++ plugins/vue-gtag-next.client.ts
@@ -3,7 +3,7 @@ import VueGtag from "vue-gtag-next";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: "UA-197110880-2",
+      id: "G-RPQJNZBZTK",
       params: {
         anonymize_ip: true,
       },
